c0ae64b3a769b026d63f9d73c7c6bba436b1e9d7
Park Jin <qkrwls6521@gmail.com>
2022-12-14 12:01:34 +0900

    add: mongoose connect

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const routes = require("./routes")
-const app = express()
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
 
-app.use('/api', routes);
+app.use("/api", routes);
+
+mongoose.connect("mongodb://localhost:27017/test");
 
 app.listen(3000, () => {
-  console.log('app listen on port 3000')
-})
+  console.log("app listen on port 3000");
+});
