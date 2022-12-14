758bcb735c4bb17a5f7f413800b1ea955d0111ad
Park Jin <qkrwls6521@gmail.com>
2022-12-14 10:30:59 +0900

    fix: route detail

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world!')
-})
+/**
+ * 댓글 목록 조회 API
+ * - 제목, 작성자명, 작성 내용을 조회하기
+ */
+router.get("/:postId", (req, res) => {});
 
-module.exports = router
+/**
+ * 댓글 작성 API
+ * - 댓글 내용을 비워둔 채 댓글 작성 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
+ * - 댓글 내용을 입력하고 댓글 작성 API를 호출한 경우 작성한 댓글을 추가하기
+ */
+router.post("/", (req, res) => {});
+
+/**
+ * 댓글 수정 API
+ * - 댓글 내용을 비워둔 채 댓글 수정 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
+ * - 댓글 내용을 입력하고 댓글 수정 API를 호출한 경우 작성한 댓글을 수정하기
+ */
+router.put("/:commentId", (req, res) => {});
+/**
+ * 댓글 삭제 API
+ */
+router.delete("/:commentId", (req, res) => {});
+
+module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world!')
-})
+/**
+ * 게시글 조회 API
+ * - 제목, 작성자명, 작성 내용을 조회하기
+ */
+router.get("/:postId", (req, res) => {});
 
-module.exports = router
+/**
+ * 전체 게시글 목록 조회 API
+ * - 제목, 작성자명, 작성 날짜를 조회하기
+ * - 작성 날짜를 기준으로 내림차순 정렬하기
+ */
+router.get("/", (req, res) => {});
+
+/**
+ * 게시글 작성 API
+ * - 제목, 작성자명, 비밀번호, 작성 내용을 입력하기
+ */
+router.post("/", (req, res) => {});
+
+/**
+ * 게시글 수정 API
+ * - 입력된 비밀번호를 확인하여 수정
+ */
+router.put("/:postId", (req, res) => {});
+/**
+ * 게시글 삭제 API
+ * - 입력된 비밀번호를 확인하여 삭제
+ */
+router.delete("/:postId", (req, res) => {});
+
+module.exports = router;
