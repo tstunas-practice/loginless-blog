1ed0accde4606dff5f6e9d712b65015493aa2741
Park Jin <qkrwls6521@gmail.com>
2022-12-14 15:19:01 +0900

    add: route detail

--- routes/comments.js
+++ routes/comments.js
@@ -1,28 +1,57 @@
 const express = require("express");
+const { Comment } = require("../schemas");
 const router = express.Router();
 
 /**
  * 댓글 목록 조회 API
- * - 제목, 작성자명, 작성 내용을 조회하기
  */
-router.get("/:postId", (req, res) => {});
+router.get("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const result = await Comment.find().where("postId").equals(postId).lean();
+  res.json(result);
+});
 
 /**
  * 댓글 작성 API
  * - 댓글 내용을 비워둔 채 댓글 작성 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
  * - 댓글 내용을 입력하고 댓글 작성 API를 호출한 경우 작성한 댓글을 추가하기
  */
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const { content, postId } = req.body;
+  const comment = new Comment({
+    content,
+    postId,
+  });
+  await comment.save();
+  res.json({
+    message: "성공",
+  });
+});
 
 /**
  * 댓글 수정 API
  * - 댓글 내용을 비워둔 채 댓글 수정 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
  * - 댓글 내용을 입력하고 댓글 수정 API를 호출한 경우 작성한 댓글을 수정하기
  */
-router.put("/:commentId", (req, res) => {});
+router.put("/:commentId", async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+  await Comment.findByIdAndUpdate(commentId, {
+    $set: { content: content },
+  }).lean();
+  res.json({
+    message: "성공",
+  });
+});
 /**
  * 댓글 삭제 API
  */
-router.delete("/:commentId", (req, res) => {});
+router.delete("/:commentId", async (req, res) => {
+  const { commentId } = req.params;
+  await Comment.findByIdAndDelete(commentId).lean();
+  res.json({
+    message: "성공",
+  });
+});
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,68 @@
 const express = require("express");
+const { Post } = require("../schemas");
 const router = express.Router();
 
 /**
  * 게시글 조회 API
  * - 제목, 작성자명, 작성 내용을 조회하기
  */
-router.get("/:postId", (req, res) => {});
+router.get("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const result = await Post.findById(postId).lean();
+  res.json(result);
+});
 
 /**
  * 전체 게시글 목록 조회 API
  * - 제목, 작성자명, 작성 날짜를 조회하기
  * - 작성 날짜를 기준으로 내림차순 정렬하기
  */
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  const result = await Post.find().sort("-createdAt").lean();
+  res.json(result);
+});
 
 /**
  * 게시글 작성 API
  * - 제목, 작성자명, 비밀번호, 작성 내용을 입력하기
  */
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const { title, author, password, content } = req.body;
+  const post = new Post({
+    title: title,
+    author: author,
+    password: password, // 추후 암호화 필요
+    content: content,
+  });
+  await post.save();
+  res.json({
+    message: "성공",
+  });
+});
 
 /**
  * 게시글 수정 API
  * - 입력된 비밀번호를 확인하여 수정
  */
-router.put("/:postId", (req, res) => {});
+router.put("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const { title, author, password, content } = req.body;
+  // TODO: 비밀번호 비교 추가
+  await Post.findByIdAndUpdate(postId, {
+    $set: { title: title, author: author, content: content },
+  }).lean();
+});
+
 /**
  * 게시글 삭제 API
  * - 입력된 비밀번호를 확인하여 삭제
  */
-router.delete("/:postId", (req, res) => {});
+router.delete("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  await Post.findByIdAndDelete(postId).lean();
+  res.json({
+    message: "성공",
+  });
+});
 
 module.exports = router;
