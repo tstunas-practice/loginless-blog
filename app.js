44b6485947736962c6ceb9fd432f024afdd322ec
Park Jin <qkrwls6521@gmail.com>
2022-12-14 12:16:13 +0900

    add: express built-in middleware express.json(), express.urlencoded()

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes);
 
